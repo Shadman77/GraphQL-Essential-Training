74adb96c77cc3805ce78a06b17b80e2ce28f968f
Shadman Saif <shadman.saif@arenaphonebd.net>
2022-10-01 16:10:09 +0600

    mongo connect

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -1,1 +1,37 @@
+// Password for mongodb
 // 6oWJYdJqr0l0ILPf
+
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb+srv://shadman_saif:6oWJYdJqr0l0ILPf@data.vcbev.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+const widgetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  soldout: {
+    type: String,
+  },
+  inventory: {
+    type: String,
+  },
+  stores: {
+    type: Array,
+  },
+});
+
+const Widgets = mongoose.model("widgets", widgetSchema);
+
+export { Widgets };
